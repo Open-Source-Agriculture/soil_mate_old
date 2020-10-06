489f6d84f83de5a13e6380cd1b57c31dedc80920
kipling <kip.crossing@gmail.com>
2020-10-06 22:52:59 +1100

    add getData method to samples

--- lib/models/sample.dart
+++ lib/models/sample.dart
@@ -1,3 +1,5 @@
+import 'package:texture_app/models/common_keys.dart';
+
 class Sample{
   final double lat;
   final double lon;
@@ -10,5 +12,18 @@ class Sample{
   final int silt;
   final int clay;
   Sample({this.lat, this.lon,this.textureClass, this.depthShallow, this.depthDeep, this.sand, this.silt, this.clay});
+
+  Map<String,dynamic> getData(){
+    return {
+      LAT:this.lat,
+      LON:this.lon,
+      TEXTURECLASS: this.textureClass,
+      DEPTHSHALLOW:this.depthShallow,
+      DEPTHDEEP:this.depthDeep,
+      SAND:this.sand,
+      SILT:this.silt,
+      CLAY: this.clay
+    };
+  }
 
 }
