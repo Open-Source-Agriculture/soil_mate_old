67715ba6181b5f39e9df5ea1da77956e43ea8360
kipling <kip.crossing@gmail.com>
2020-10-07 16:05:34 +1100

    Load sites to card list

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -7,12 +7,28 @@ import 'package:texture_app/services/site_database.dart';
 import 'package:texture_app/models/site.dart';
 import 'package:texture_app/models/common_keys.dart';
 
+
 class Home extends StatelessWidget {
 
   final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
+
+
+//    Site iSite = Site(
+//        name: "Heph Paddock",
+//        classification: "aus",
+//        rawSamples: []
+//    );
+//    bool alreadySite = saveSite(iSite);
+//    if (alreadySite){
+//      print("Cant use this name; already exists");
+//    }
+    List<Site> allSites =  getSites();
+    print(allSites);
+    List<dynamic> allSitesNames = allSites.map((s) => s.name).toList();
+    print(allSitesNames);
 
     return Container(
       child: Scaffold(
@@ -30,29 +46,17 @@ class Home extends StatelessWidget {
           ],
         ),
         body: ListView.builder(
-            // itemCount: locations.length,
+            itemCount: allSites.length,
             itemBuilder: (context, index){
               return Padding(
                 padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
                 child: Card(
                   child: ListTile(
                     onTap: () {
-                      Site iSite = Site(
-                        name: "Bobs Paddock",
-                        classification: "aus",
-                        rawSamples: []
-                      );
-                      bool alreadySite = saveSite(iSite);
-                      if (alreadySite){
-                        print("Cant use this name; already exists");
-                      }
-//                      List<Site> allSites =  getSites();
-//                      print(allSites);
-//                      List<String> allSitesNames = allSites.map((s) => s.name).toList();
-//                      print(allSitesNames);
+
                     },
                     // title: Text(locations[index].location),
-                    title: Text("Site One"),
+                    title: Text(allSites[index].name),
                     /*leading: Text('1') CircleAvatar(
                       backgroundImage: AssetImage('assets/${locations[index].flag}'),
                     ),*/
