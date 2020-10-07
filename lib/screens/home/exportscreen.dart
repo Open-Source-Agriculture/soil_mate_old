e31ebf474c6d9112ba4a7ae90d7016a87e6a4f59
otterSp0tter <hep.crossing@gmail.com>
2020-10-07 11:39:19 +1100

    populated the export screen with check box widgets and flat button. nothing happens on pressed yet.

--- lib/screens/home/exportscreen.dart
+++ lib/screens/home/exportscreen.dart
@@ -6,11 +6,83 @@ class ExportScreen extends StatefulWidget {
 }
 
 class _ExportScreenState extends State<ExportScreen> {
+  bool selectDataAll = false;
+  bool selectDataBySite = false;
+  bool selectDataByDate = false;
+  bool exportByEmail = false;
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
-        body: Text('this is export screen'),
+        primary: true,
+        appBar: new AppBar(
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text('Which Samples do you want to export?'),
+              Row(
+                children: [
+                  Checkbox(
+                    value: selectDataAll,
+                    onChanged: (bool value) {
+                      setState(() {
+                        selectDataAll = value;
+                      });
+                    },
+                  ),
+                  Text('All'),
+                ],
+              ),
+              Row(
+                children: [
+                  Checkbox(
+                    value: selectDataByDate,
+                    onChanged: (bool value) {
+                      setState(() {
+                        selectDataByDate = value;
+                      });
+                    },
+                  ),
+                  Text('Select By Date'),
+                ],
+              ),
+              Row(
+                children: [
+                  Checkbox(
+                    value: selectDataBySite,
+                    onChanged: (bool value) {
+                      setState(() {
+                        selectDataBySite = value;
+                      });
+                    },
+                  ),
+                  Text('Select by Site'),
+                ],
+              ),
+              Text('How do you want to export?'),
+              Row(
+                children: [
+                  Checkbox(
+                    value: exportByEmail,
+                    onChanged: (bool value) {
+                      setState(() {
+                        exportByEmail = value;
+                      });
+                    },
+                  ),
+                  Text('Export by Email'),
+                ],
+              ),
+              FlatButton(
+                  onPressed: (){},
+                  child:
+                  Text('Export'),)
+            ],
+          ),
+        ),
       ),
     );
   }
